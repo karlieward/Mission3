b0732d88c164f1c9532af624bb663ba7877d4642
karlieward <karlieward3@gmail.com>
2026-01-21 13:42:11 -0700

    Initial commit

---
+++ FoodItem.cs
@@ -0,0 +1,33 @@
+namespace Mission3;
+
+public class FoodItem
+{
+    string name;
+    string category;
+    private int quantity;
+    private DateTime expirationDate;
+
+
+    public AddFoodItem(string name, string category, int quantity, DateTime expirationDate)
+    {
+        
+    }
+
+    public DeleteFoodItem()
+    {
+        
+    }
+
+    public PrintList()
+    {
+    }
+
+    public Exit()
+    {
+        Console.WriteLine("Goodbye");
+    }
+
+
+
+}
+
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using Mission3;
+
+FoodItem fi = new FoodItem;
+
+
+Console.WriteLine("Choose a number to use the menu: ");
+Console.WriteLine("1. Add Food Items");
+Console.WriteLine("2. Delete Food Items");
+Console.WriteLine("3. Print List of Current Food Items");
+Console.WriteLine("4. Exit Program");
+
+int userChoice = int.Parse(Console.ReadLine());
+        
